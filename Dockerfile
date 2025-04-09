3cb0f30406588b2c0c34013b45fc8b69eb9900be
jack drabenstadt <jackdrabenstadt@gmail.com>
2025-04-09 12:52:45 -0400

    recovered old docker implemenations

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN dnf install -y vim make gcc iproute-tc
 # Install Spire dependencies
 RUN dnf install -y dnf-plugins-core
 RUN dnf config-manager --set-enabled crb
-RUN dnf install -y openssl-devel flex byacc qt5-devel cmake python
+RUN dnf install -y openssl-devel flex byacc qt5-devel cmake python git libyaml-devel
 
 # Install debugging tools
 RUN dnf install -y gdb valgrind
@@ -18,10 +18,16 @@ WORKDIR /app/spire
 # Set up config files
 RUN cd example_conf; ./install_conf.sh conf_4 
 
+# Install libcyaml from source
+RUN git clone https://github.com/tlsa/libcyaml.git /tmp/libcyaml && \
+    cd /tmp/libcyaml && \
+    make && make install && \
+    ldconfig
+
 # Build Spire core (Spines, Prime, SCADA Master, benchmark)
 RUN make core
 
 # Run the script to check and generate keys if needed
-RUN python3 /app/spire/check_keys.py
+RUN python3 /app/spire/run_spire.py
 
 #ENTRYPOINT /bin/bash
---
+++ run_spire.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import subprocess
+
+def run_check_keys():
+    print("Running key check script...")
+    subprocess.run(["python3", "/app/spire/check_keys.py"], check=True)
+
+def run_replica(replica_id):
+    print(f"Starting Spire Replica {replica_id}...")
+
+    spines_int_cmd = f"cd /app/spire/spines/daemon && ./spines -p 8100 -c spines_int.conf -I 192.168.101.10{replica_id}"
+    spines_ext_cmd = f"cd /app/spire/spines/daemon && ./spines -p 8120 -c spines_ext.conf -I 192.168.101.10{replica_id}"
+    sm_cmd = f"cd /app/spire/scada_master && ./scada_master {replica_id} {replica_id} 192.168.101.10{replica_id}:8100 192.168.101.10{replica_id}:8120"
+    prime_cmd = f"cd /app/spire/prime/bin && ./prime -i {replica_id} -g {replica_id}"
+
+    sp_proc = subprocess.Popen(spines_int_cmd, shell=True)
+    subprocess.Popen(spines_ext_cmd, shell=True)
+    subprocess.Popen(sm_cmd, shell=True)
+    subprocess.Popen(prime_cmd, shell=True)
+
+    sp_proc.communicate()
+
+def run_client():
+    print("Starting Spire Client...")
+    spines_ext_cmd = "cd /app/spire/spines/daemon && ./spines -p 8120 -c spines_ext.conf -I 192.168.101.107"
+    sp_proc = subprocess.Popen(spines_ext_cmd, shell=True)
+    sp_proc.communicate()
+
+def run_conf_agent():
+    print("Starting Config Agent...")
+    spines_cmd = "cd /app/spire/spines/daemon && ./spines -p 8100 -c spines_int.conf -I 192.168.101.201"
+    agent_cmd = "cd /app/spire && ./bin/config_receiver"
+
+    sp_proc = subprocess.Popen(spines_cmd, shell=True)
+    subprocess.Popen(agent_cmd, shell=True)
+
+    sp_proc.communicate()
+
+def run_conf_manager():
+    print("Starting Config Manager...")
+    spines_cmd = "cd /app/spire/spines/daemon && ./spines -p 8100 -c spines_int.conf -I 192.168.101.202"
+
+    sp_proc = subprocess.Popen(spines_cmd, shell=True)
+    sp_proc.communicate()
+
+if __name__ == "__main__":
+    run_check_keys()
+
+    mode = os.getenv("SPIRE_MODE", "replica")
+    if mode == "replica":
+        spire_id = os.getenv("SPIRE_ID")
+        if not spire_id:
+            print("ERROR: SPIRE_ID must be set for replicas!")
+            sys.exit(1)
+        run_replica(spire_id)
+    elif mode == "client":
+        run_client()
+    elif mode == "conf_agent":
+        run_conf_agent()
+    elif mode == "conf_manager":
+        run_conf_manager()
+    else:
+        print("ERROR: Unknown SPIRE_MODE! Must be 'replica', 'client', 'conf_agent', or 'conf_manager'.")
+        sys.exit(1)
